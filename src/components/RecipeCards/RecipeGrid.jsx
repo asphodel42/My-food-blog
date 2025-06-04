0a5e6516704e627cfe6d2042a9ef379c7e0377a4
asphodel42 <asphodell42@gmail.com>
2025-06-05 02:58:11 +0300

    created RecipeGrid component and added styles + responcivnes

---
+++ src/components/RecipeCards/RecipeGrid.jsx
@@ -0,0 +1,52 @@
+import RecipeCard from "./RecipeCard";
+import { recipes } from "../../data/recipes.js";
+import PaginationSlider from "./PaginationSlider";
+import { useState, useRef } from "react";
+import "./Recipe.css";
+
+let ITEMS_PER_PAGE = 8;
+
+function RecipeGrid() {
+  if (window.innerWidth <= 600) {
+    ITEMS_PER_PAGE = 6;
+  } else if (window.innerWidth <= 800) {
+    ITEMS_PER_PAGE = 4;
+  } else if (window.innerWidth <= 1200) {
+    ITEMS_PER_PAGE = 6;
+  } else {
+    ITEMS_PER_PAGE = 8;
+  }
+  const [page, setPage] = useState(1);
+  const totalPages = Math.ceil(recipes.length / ITEMS_PER_PAGE);
+
+  const startIndex = (page - 1) * ITEMS_PER_PAGE;
+  const visibleItems = recipes.slice(startIndex, startIndex + ITEMS_PER_PAGE);
+
+  const gridRef = useRef(null);
+
+  const handlePageChange = (newPage) => {
+    setPage(newPage);
+    setTimeout(() => {
+      if (gridRef.current) {
+        gridRef.current.scrollIntoView({ behavior: "smooth" });
+      }
+    }, 50);
+  };
+
+  return (
+    <>
+      <div ref={gridRef} className="recipe-grid">
+        {visibleItems.map((recipe) => (
+          <RecipeCard key={recipe.id} {...recipe} />
+        ))}
+      </div>
+      <PaginationSlider
+        page={page}
+        setPage={handlePageChange}
+        totalPages={totalPages}
+      />
+    </>
+  );
+}
+
+export default RecipeGrid;
