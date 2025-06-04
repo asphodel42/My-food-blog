d9b5e377faaf27bc510f9d24b01be331ad4f87eb
asphodel42 <asphodell42@gmail.com>
2025-06-05 02:58:26 +0300

    added data for recipe cards

---
+++ src/data/recipes.js
@@ -0,0 +1,88 @@
+export const recipes = [
+  {
+    id: 1,
+    title: "The Perfect Sandwich, A Real NYC Classic",
+    description: "Just some random text lorem ipsum.",
+    image: "./src/assets/images/sandwich.jpg",
+  },
+  {
+    id: 2,
+    title: "Let Me Tell You About This Steak",
+    description: "Once again, some random text lorem ipsum.",
+    image: "./src/assets/images/steak.jpg",
+  },
+  {
+    id: 3,
+    title: "Cherries, interrupted",
+    description: "Lorem ipsum text praesent tincidunt ipsum ipsum. What else?",
+    image: "./src/assets/images/cherries.jpg",
+  },
+  {
+    id: 4,
+    title: "Once Again, Robust Wine and Vegetable Pasta",
+    description: "Lorem ipsum text praesent tincidunt ipsum ipsum.",
+    image: "./src/assets/images/wine.jpg",
+  },
+  {
+    id: 5,
+    title: "All I Need Is a Popsicle",
+    description: "Lorem ipsum text praesent tincidunt ipsum ipsum.",
+    image: "./src/assets/images/popsicle.jpg",
+  },
+  {
+    id: 6,
+    title: "Salmon For Your Skin",
+    description:
+      "Once again, some random text to lorem lorem lorem lorem ipsum text praesent tincidunt ipsum ipsum.",
+    image: "./src/assets/images/salmon.jpg",
+  },
+  {
+    id: 7,
+    title: "The Perfect Sandwich, A Real Classic",
+    description:
+      "Just some random text. Lorem ipsum text praesent tincidunt ipsum ipsum.",
+    image: "./src/assets/images/steak.jpg",
+  },
+  {
+    id: 8,
+    title: "Le French",
+    description: "Lorem ipsum text praesent tincidunt ipsum ipsum.",
+    image: "./src/assets/images/sandwich.jpg",
+  },
+  {
+    id: 9,
+    title: "Le French",
+    description: "Lorem ipsum text praesent tincidunt ipsum ipsum.",
+    image: "./src/assets/images/croissant.jpg",
+  },
+  {
+    id: 10,
+    title: "Cherries, interrupted",
+    description: "Lorem ipsum text praesent tincidunt ipsum ipsum. What else?",
+    image: "./src/assets/images/cherries.jpg",
+  },
+  {
+    id: 11,
+    title: "Once Again, Robust Wine and Vegetable Pasta",
+    description: "Lorem ipsum text praesent tincidunt ipsum ipsum.",
+    image: "./src/assets/images/wine.jpg",
+  },
+  {
+    id: 12,
+    title: "All I Need Is a Popsicle",
+    description: "Lorem ipsum text praesent tincidunt ipsum ipsum.",
+    image: "./src/assets/images/popsicle.jpg",
+  },
+  {
+    id: 3,
+    title: "The Perfect Sandwich, A Real NYC Classic",
+    description: "Just some random text lorem ipsum.",
+    image: "./src/assets/images/sandwich.jpg",
+  },
+  {
+    id: 14,
+    title: "Let Me Tell You About This Steak",
+    description: "Once again, some random text lorem ipsum.",
+    image: "./src/assets/images/steak.jpg",
+  },
+];
