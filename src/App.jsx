08a6cb2f7554764a26bc774fa8a08513cd9e4a8f
asphodel42 <asphodell42@gmail.com>
2025-06-05 02:58:45 +0300

    created basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
-function App() {}
+import AboutBlock from "./components/AboutBlock/AboutBlock.jsx";
+import RecipeGrid from "./components/RecipeCards/RecipeGrid.jsx";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <main>
+        <RecipeGrid></RecipeGrid>
+        <AboutBlock></AboutBlock>
+      </main>
+    </>
+  );
+}
 
 export default App;
